2f8bb29eb05298778ebcf999e4a7017704e1d80b
voyagerwoo <voyager.woo@gmail.com>
2018-08-01 01:56:33 +0900

    Add aws cli lambda invoke script

---
+++ lambda_invoke.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+aws lambda invoke \
+--invocation-type RequestResponse \
+--function-name pos-tagger \
+--region ap-northeast-1 \
+--payload "\"$1\"" \
+outputfile.txt
+
+cat outputfile.txt
